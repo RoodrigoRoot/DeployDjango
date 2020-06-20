b0399d79b3d88876bccc0fee49a57aa29827d82f
roodrigo <roodrigoroot@gmtail.com>
2020-06-20 01:23:15 -0500

    db

--- web/config/settings.py
+++ web/config/settings.py
@@ -84,15 +84,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-""" 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} """
+}
 
-DATABASES = {
+""" DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
          'NAME': 'autopartes',
@@ -102,7 +102,7 @@ DATABASES = {
          'PORT': '5432',
      }
  }
-
+ """
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
